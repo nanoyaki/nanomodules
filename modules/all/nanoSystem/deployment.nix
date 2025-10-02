c2aee7443530e5f4c2299ebce0dc115ec0906baf
nanoyaki <hanakretzer@nanoyaki.space>
2025-10-02 19:23:20 +0200

    fix: deployment lib.mkEnableOption inheritance, norgb -> no-rgb

--- modules/all/nanoSystem/deployment.nix
+++ modules/all/nanoSystem/deployment.nix
@@ -12,6 +12,7 @@ let
     nameValuePair
     literalExpression
     mkIf
+    mkEnableOption
     ;
 
   cfg = config.nanoSystem.deployment;
@@ -23,33 +24,30 @@ in
 
     addresses = mkOption {
       type = types.attrsOf (
-        types.submodule (
-          { name, ... }:
-          {
-            options = {
-              targetUser = mkOption {
-                type = types.str;
-                default = "";
-                example = "root";
-                description = ''
-                  The user to log into over ssh. It's recommended
-                  to use the root user or a user that has trusted
-                  access to the nix store.
-                '';
-              };
-
-              publicKey = mkOption {
-                type = with types; either pathInStore str;
-                default = "";
-                example = literalExpression ''./id_ed25519.pub'';
-                description = ''
-                  The public key that is allowed to deploy to
-                  <targetUser>@<host>
-                '';
-              };
+        types.submodule {
+          options = {
+            targetUser = mkOption {
+              type = types.str;
+              default = "";
+              example = "root";
+              description = ''
+                The user to log into over ssh. It's recommended
+                to use the root user or a user that has trusted
+                access to the nix store.
+              '';
             };
-          }
-        )
+
+            publicKey = mkOption {
+              type = with types; either pathInStore str;
+              default = "";
+              example = literalExpression ''./id_ed25519.pub'';
+              description = ''
+                The public key that is allowed to deploy to
+                <targetUser>@<host>
+              '';
+            };
+          };
+        }
       );
     };
   };
